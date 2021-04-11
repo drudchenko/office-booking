9c90cc6bba2f256c63ab9ed7bb5c425f3cd5eecf
Denys Rudchenko <deniskinrud@yahoo.com>
2021-04-11 15:07:13 +0200

    Changing Booking repository to use JPA

---
+++ src/main/java/org/denysr/learning/office_booking/infrastructure/jpa/booking/BookingRepositoryJpaImpl.java
@@ -0,0 +1,48 @@
+package org.denysr.learning.office_booking.infrastructure.jpa.booking;
+
+import lombok.RequiredArgsConstructor;
+import org.denysr.learning.office_booking.domain.booking.Booking;
+import org.denysr.learning.office_booking.domain.booking.BookingId;
+import org.denysr.learning.office_booking.domain.booking.BookingRepository;
+import org.denysr.learning.office_booking.domain.booking.BusinessWeek;
+import org.denysr.learning.office_booking.domain.user.User;
+import org.denysr.learning.office_booking.domain.validation.EntityNotFoundException;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@Repository
+public class BookingRepositoryJpaImpl implements BookingRepository {
+    private final JpaBookingRepository jpaBookingRepository;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public BookingId saveBooking(Booking booking) throws EntityNotFoundException {
+        final int bookingId = jpaBookingRepository
+                .save(modelMapper.map(booking, BookingJpaDto.class))
+                .getBookingId();
+        return new BookingId(bookingId);
+    }
+
+    @Override
+    public List<Booking> getBookingsForWeek(BusinessWeek businessWeek) {
+        return modelMapper.map(jpaBookingRepository.getBookingJpaDtosByStartDateBetweenOrEndDateBetween(
+                        businessWeek.getBusinessWeekStart(), businessWeek.getBusinessWeekEnd(),
+                        businessWeek.getBusinessWeekStart(), businessWeek.getBusinessWeekEnd()
+                ), new TypeToken<List<Booking>>() {}.getType());
+    }
+
+    @Override
+    public void deleteBooking(BookingId bookingId) throws EntityNotFoundException {
+        try {
+            jpaBookingRepository.deleteById(bookingId.getBookingId());
+        } catch (EmptyResultDataAccessException e) {
+            throw new EntityNotFoundException("Booking with the mentioned id not found", e);
+        }
+    }
+}
--- src/main/java/org/denysr/learning/office_booking/infrastructure/jpa/booking/JpaBookingRepository.java
+++ src/main/java/org/denysr/learning/office_booking/infrastructure/jpa/booking/JpaBookingRepository.java
@@ -2,5 +2,14 @@ package org.denysr.learning.office_booking.infrastructure.jpa.booking;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface JpaBookingRepository extends JpaRepository<BookingJpaDto, Integer> {
+    List<BookingJpaDto> getBookingJpaDtosByStartDateBetweenOrEndDateBetween(
+            LocalDate startDateLowerValue,
+            LocalDate startDateUpperValue,
+            LocalDate endDateLowerValue,
+            LocalDate endDateUpperValue
+    );
 }
