5cc007c0a1f401a1d820b346fdbf8549cc5279fa
Denys Rudchenko <deniskinrud@gmail.com>
2025-04-30 15:40:03 +0200

    Removing redundant mocks Updating libraries

--- src/test/java/org/denysr/learning/office_booking/controllers/UserControllerIT.java
+++ src/test/java/org/denysr/learning/office_booking/controllers/UserControllerIT.java
@@ -10,8 +10,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Arrays;
@@ -31,7 +31,7 @@ class UserControllerIT {
 
     @Autowired
     private MockMvc mvc;
-    @MockBean
+    @MockitoBean
     private UserRepository userRepository;
     private ObjectMapper objectMapper = new ObjectMapper();
 
