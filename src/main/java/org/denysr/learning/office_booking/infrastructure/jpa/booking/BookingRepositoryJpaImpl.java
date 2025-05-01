8f9f7ba05a85431ee1e4b5caf962daf4675b306b
Denys Rudchenko <deniskinrud@gmail.com>
2025-05-01 15:14:39 +0200

    Removing unused imports

--- src/main/java/org/denysr/learning/office_booking/infrastructure/jpa/booking/BookingRepositoryJpaImpl.java
+++ src/main/java/org/denysr/learning/office_booking/infrastructure/jpa/booking/BookingRepositoryJpaImpl.java
@@ -5,7 +5,6 @@ import org.denysr.learning.office_booking.domain.booking.Booking;
 import org.denysr.learning.office_booking.domain.booking.BookingId;
 import org.denysr.learning.office_booking.domain.booking.BookingRepository;
 import org.denysr.learning.office_booking.domain.booking.BusinessWeek;
-import org.denysr.learning.office_booking.domain.user.User;
 import org.denysr.learning.office_booking.domain.validation.EntityNotFoundException;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeToken;
@@ -13,7 +12,6 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Repository
